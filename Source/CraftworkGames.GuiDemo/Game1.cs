248e3c9d165d4473a1ba1142061e0809e322930d
Dylan Wilson <dylan@craftworkgames.com>
2013-09-28 21:30:35 +1000

    Added an animated background to the demo.

--- Source/CraftworkGames.GuiDemo/Game1.cs
+++ Source/CraftworkGames.GuiDemo/Game1.cs
@@ -18,6 +18,8 @@ namespace CraftworkGames.GuiDemo
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private GuiSystem _gui;
+        private Texture2D _backgroundTexture;
+        private Texture2D _blurredRectangle;
 
         public Game1(bool isFullScreen = false)
         {
@@ -35,6 +37,8 @@ namespace CraftworkGames.GuiDemo
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _backgroundTexture = Content.Load<Texture2D>("Background");
+            _blurredRectangle = Content.Load<Texture2D>("BlurredRectangle");
 
             Directory.SetCurrentDirectory(Content.RootDirectory);
             var fontFile = "ExampleFont.fnt";
@@ -46,8 +50,8 @@ namespace CraftworkGames.GuiDemo
 
             var layout = new DockLayout();
             
-            var buttonOnTexture = Content.Load<Texture2D>("ButtonSwitchOn.png");
-            var buttonOffTexture = Content.Load<Texture2D>("ButtonSwitchOff.png");
+            var buttonOnTexture = Content.Load<Texture2D>("ButtonSwitchOn");
+            var buttonOffTexture = Content.Load<Texture2D>("ButtonSwitchOff");
             var textureRegionOn = new TextureRegion(buttonOnTexture);
             var textureRegionOff = new TextureRegion(buttonOffTexture);
 
@@ -101,18 +105,29 @@ namespace CraftworkGames.GuiDemo
             // For Mobile devices, this logic will close the Game when the Back button is pressed
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 Exit();
+            
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            var deltaTime = (float)gameTime.ElapsedGameTime.Seconds;
-
+            _rotation += deltaTime * 0.1f;
             _gui.Update(deltaTime);
 
             base.Update(gameTime);
         }
 
+        private float _rotation = 0;
         protected override void Draw(GameTime gameTime)
         {
             _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
-		
+
+            var position = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            var rotation = _rotation;
+            var origin = new Vector2(_blurredRectangle.Width / 2, _blurredRectangle.Height / 2);
+            var scale = 1f;
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
+            _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+            _spriteBatch.Draw(_blurredRectangle, position, null, new Color(Color.White, 0.25f), rotation, origin, scale, SpriteEffects.None, 0);
+            _spriteBatch.End();
+
             _gui.Draw();
             
             base.Draw(gameTime);
